dfefb894f6572717e6dc95bd7cd13acdb563501c
Ethan C. <98814948+EthanCatton@users.noreply.github.com>
2026-01-30 21:31:00 +0000

    ui recolour
    ui recolour to be midly more bareable, I wish I knew frontend....

--- Web/Frontend/React/Web-React/src/App.tsx
+++ Web/Frontend/React/Web-React/src/App.tsx
@@ -5,14 +5,7 @@ function App() {
   // function getquery() {}
   return (
     <>
-      <header className="headingbar">
-        Imagine like a really cool name here wow isnt it amazing
-      </header>
       <div className="background"></div>
-
-      <div className="sidebar">
-        <div className="sidebar_item">wip</div>
-      </div>
 
       <div className="center">
         <Search />
